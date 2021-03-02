5e9d8acbe4d21db2603875428c4353e081adc0d8
kmou424 <dx2689282850@gmail.com>
2021-03-02 23:11:28 +0800

    logx: Use environment variables to replace OUTPUT&TAG parameters
    Signed-off-by: kmou424 <dx2689282850@gmail.com>

--- logx.c
+++ logx.c
@@ -5,11 +5,14 @@
 
 #define LOGX_VERSION "1.0"
 
+const char *LOGX_OUT = "LOGXOUT";
+const char *LOGX_TAG = "LOGXTAG";
+
 char *attach(char *, char*);
 
 int main(int argc,char *argv[])
 {
-    if(argc > 4){
+    if(argc > 2){
         printf("logx: Too much parameters\n");
         exit (1);
     } else if (argc == 2){
@@ -20,22 +23,39 @@ int main(int argc,char *argv[])
             printf("logx: Usage:\n      logx <log file path> <TAG> <log>\n");
             exit (0);
         }
-    } else if (argc < 4){
-        printf("logx: Too few parameters\n");
+    } else if (argc < 2){
+        printf("logx: Use \"logx -h\" or \"logx help\" to get help information\n");
         exit (1);
     }
+    
+    char *logx_out = getenv(LOGX_OUT);
+    char *logx_tag = getenv(LOGX_TAG);
+    int env_status = 0;
+    if(logx_out == NULL){
+      printf("logx: error: %s is %s\nPlease use \"export %s=<Output file path>\" to set this environment variable\n", LOGX_OUT, logx_out, LOGX_OUT);
+      env_status = -1;
+    }
+    if(logx_tag == NULL){
+      printf("logx: error: %s is %s\nPlease use \"export %s=<TAG of log sender>\" to set this environment variable\n", LOGX_TAG, logx_tag, LOGX_TAG);
+      env_status = -1;
+    }
+    if(env_status){
+      printf("\nlogx: Some environment variables are not set, please fix above errors and retry\n");
+      exit(1);
+    }
+    
     char buff[20];
     time_t now = time(NULL);
     strftime(buff, 20, "%H:%M:%S", localtime(&now));
 	
 	FILE *fp;
-    if((fp=fopen(argv[1],"a+"))==NULL)
+    if((fp=fopen(logx_out, "a+"))==NULL)
     {
-        printf("logx: Wrong parameters, can not open file\n");
+        printf("logx: Can not open \"%s\", please check your %s environment variable\n", logx_out, LOGX_OUT);
         exit(1);
     }
 
-	char* append = attach(attach("[",attach(&buff,"]")),attach(attach(" ", attach(argv[2], attach(": ", argv[3]))),"\n"));
+	char* append = attach(attach("[",attach(&buff,"]")),attach(attach(" ", attach(logx_tag, attach(": ", argv[1]))),"\n"));
 	fputs(append,fp);
 	fclose(fp);
 	return 0;
